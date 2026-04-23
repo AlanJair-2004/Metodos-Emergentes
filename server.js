5114b965b876cf313304109dde38f3e840ffbbb8
Cristian Backend <cristianbackend@gmail.com>
2026-04-23 10:57:28 -0500

    Agregue la funcion de Qr

--- server.js
+++ server.js
@@ -15,3 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/qrs', express.static(path.join(__dirname, 'qrs')));
+
+function generarCodigoQR(matricula) {
+  return `QR-${matricula}-${Date.now()}`;
+}
