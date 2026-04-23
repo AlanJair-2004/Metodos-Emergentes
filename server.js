706b5919827d077d01f244d995a38088155ccb45
Cristian Backend <cristianbackend@gmail.com>
2026-04-23 10:55:20 -0500

    Dependencias y arranque

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
+const QRCode = require('qrcode');
+const { db, initDb } = require('./db');
+const https = require('https');
+
+const app = express();
+const PORT = 3001;
+
+initDb();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/qrs', express.static(path.join(__dirname, 'qrs')));
